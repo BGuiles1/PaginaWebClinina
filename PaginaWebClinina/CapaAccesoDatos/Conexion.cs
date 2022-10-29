7e5fd6e8892d1398f0db96b26b05e5c3dbe9d0a7
GUILES-OXLAJ <GUILES-OXLAJ@BRYAN-GUILES>
2022-10-28 23:53:58 -0600

    Agregar archivos de proyecto.

---
+++ PaginaWebClinina/CapaAccesoDatos/Conexion.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaAccesoDatos
+{
+    public class Conexion
+    {
+        #region "PATRON SINGLETON"
+        private static Conexion conexion = null;
+        private Conexion() { }
+        public static Conexion getInstance()
+        {
+            if (conexion == null)
+            {
+                conexion = new Conexion();
+            }
+            return conexion;
+        }
+        #endregion
+
+        public SqlConnection ConexionBD()
+        {
+            SqlConnection conexion = new SqlConnection();
+            conexion.ConnectionString = "Data Source=SQL5104.site4now.net;Initial Catalog=db_a8cb54_desaweb2022;Persist Security Info=True;User ID=db_a8cb54_desaweb2022_admin;Password=br4928ox;MultipleActiveResultSets=True";
+            return conexion;
+        }
+    }
+}
---
+++ PaginaWebClinina/CapaDatos/Conexion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class Conexion
+    {
+        public static string CN = "Data Source=SQL5104.site4now.net;Initial Catalog=db_a8cb54_desaweb2022;Persist Security Info=True;User ID=db_a8cb54_desaweb2022_admin;Password=br4928ox;MultipleActiveResultSets=True";
+    }
+}
